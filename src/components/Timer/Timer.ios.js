680f853eccb69fb0ec54d7d5dae969d9d6002f98
Tamer Bader <italktechs@gmail.com>
2017-05-22 00:35:48 -0400

    Fixed the case of getting 00 in the time format

--- src/components/Timer/Timer.ios.js
+++ src/components/Timer/Timer.ios.js
@@ -79,7 +79,15 @@ export default class Timer extends Component {
       hours  = Math.floor(value/3600);
       minutes = Math.floor((value - (hours*3600))/60);
       seconds = Math.floor(value - (hours*3600) - (minutes*60));
-      
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
       var time = hours + ":" + minutes + ":" + seconds;
       return time;
     }
